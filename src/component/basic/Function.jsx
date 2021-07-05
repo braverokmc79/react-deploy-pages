abc307542c0b854aec05fe1a1bb9d307aef1a660
junhochoi <braverokmc79@gmail.com>
2021-07-05 17:55:03 +0900

    gitignore

---
+++ src/component/basic/Function.jsx
@@ -0,0 +1,13 @@
+import React from  'react';
+
+
+const UserComponent = (props)=>{
+
+    return(
+        <div className="container">
+            컴포넌트 내용 작성 부분
+        </div>
+    );
+}
+
+export default UserComponent
